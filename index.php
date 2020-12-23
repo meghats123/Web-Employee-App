fdea7272294fcfc10f9b4d950d2d87a63a0f9f39
meghats123 <meghats39032@gmail.com>
2020-12-23 12:48:10 +0530

    clickevent completed

--- index.php
+++ index.php
@@ -14,41 +14,62 @@
     <table class="table table-borderless">
         <tr>
             <td>Emp Code</td>
-            <td><input type="text" class="form-control"></td>
+            <td><input id="ecode" type="text" class="form-control"></td>
         </tr>
         <tr>
             <td>Emp Name</td>
-            <td><input type="text" class="form-control"></td>
+            <td><input id="ename" type="text" class="form-control"></td>
         </tr>
         <tr>
             <td>Designation</td>
-            <td><input type="text" class="form-control"></td>
+            <td><input id="desig" type="text" class="form-control"></td>
         </tr>
         <tr>
             <td>Address</td>
-            <td><input type="text" class="form-control"></td>
+            <td><input id="address" type="text" class="form-control"></td>
         </tr>
         <tr>
             <td>Mobile No</td>
-            <td><input type="text" class="form-control"></td>
+            <td><input id="mob" type="text" class="form-control"></td>
         </tr>
         <tr>
             <td>Username</td>
-            <td><input type="text" class="form-control"></td>
+            <td><input id="uname" type="text" class="form-control"></td>
         </tr>
         <tr>
             <td>Password</td>
-            <td><input type="text" class="form-control"></td>
+            <td><input id="pass" type="text" class="form-control"></td>
         </tr>
         <tr>
             <td></td>
-            <td><Button class="btn btn-primary">REGISTER</Button></td>
+            <td><Button onclick="mybuttonclick()" class="btn btn-primary">REGISTER</Button></td>
         </tr>
 
     </table>
             </div>
         </div>
     </div>
+    <script>
+    function mybuttonclick(){
+        var getEmpCode=document.getElementById("ecode").value
+        var getEmpName=document.getElementById("ename").value
+        var getDesignation=document.getElementById("desig").value
+        var getAddress=document.getElementById("address").value
+        var getMobile=document.getElementById("mob").value
+        var getUsername=document.getElementById("uname").value
+        var getPassword=document.getElementById("pass").value
+        console.log(getEmpCode)
+        console.log(getEmpName)
+        console.log(getDesignation)
+        console.log(getAddress)
+        console.log(getMobile)
+        console.log(getUsername)
+        console.log(getPassword)
+
+        
+    }
+    
+    </script>
     
 </body>
 </html>
